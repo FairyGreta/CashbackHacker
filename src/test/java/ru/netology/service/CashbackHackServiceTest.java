2387cc4692670e7dcba9001b7dfb3396c22a6b96
FairyGreta <Christina_Art@mail.ru>
2022-08-08 13:48:36 +0500

    Added tests to the branch testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,57 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldBuy300() {
+        int amount = 5700;
+        int expected = 300;
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldBuy100() {
+        int amount = 900;
+        int expected = 100;
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldBuy10() {
+        int amount = 990;
+        int expected = 10;
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateIfNoAmount() {
+        int amount = 0;
+        int expected = 1000;
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+    // тест падает, так как предлагает докупить еще на 1000
+    @Test
+    public void shouldBuy0() {
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+}
